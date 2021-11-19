f97b3b56821a699ef93d8c89195a4ab4b6f8a169
kjy <ilu52046224@gmail.com>
2021-11-19 23:20:18 +0900

    수정

--- src/barricade1,3.cpp
+++ src/barricade1,3.cpp
@@ -27,7 +27,7 @@ const int pinTrig_1 = 4;
 const int pinEcho_1 = 5;
 const int pinTrig_3 = 7;
 const int pinEcho_3 = 8;
-int rangeMax = 200;
+int rangeMax = 50;
 int rangeMin = 0;
 long T_1, T_3;
 long L_1, L_3;
@@ -56,8 +56,8 @@ void loop()
 
     if (mySerial.available() > 0) //데이터가 수신되는지 확인
     {
-        char ch = mySerial.read(); //1바이트 읽음
-        mySerial.println(ch);      //1바이트 읽은거 출력
+        c = mySerial.read(); //1바이트 읽음
+        mySerial.println(c); //1바이트 읽은거 출력
     }
 
     //초음파 보내며, 다 보내면 echo가 HIGH(신호받기) 상태로 대기
@@ -77,9 +77,7 @@ void loop()
     L_3 = T_3 / 58.82; //3번 초음파 센서로 거리 측정
 
     ///////////////////////////
-    //딜레마존 구현부
-    Time1 = 0;
-    Time3 = 0;
+    //딜레마존 구현부s
 
     //차량이 범위내에 없으면
     if (L_1 < rangeMax)
@@ -113,9 +111,10 @@ void loop()
 
     ///////////////////////////
     //바리게이트 작동 구현부
-    switch (signal)
+    //신호 받고 스위치 케이스에 넣어야됨
+    switch (c)
     {
-    case 1:
+    case '1':
         if (l2 - l1 >= interval)
         {
             l1 = l2;
@@ -135,17 +134,20 @@ void loop()
                 {
                     myservo_1.write(pos_1);
                 }
+                l1 = 0;
+                Time3 = 0;
+                break;
             }
         }
         break;
-    case 3:
+    case '3':
         if (l2 - l1 >= interval)
         {
             l1 = l2;
             Time3++;
             if (Time3 >= 1 && Time3 < 2)
             {
-                //1번 바리게이트 올라옴
+                //3번 바리게이트 올라옴
                 for (pos_3 = 0; pos_3 <= degree; pos_3 += 1)
                 {
                     myservo_3.write(pos_3);
@@ -153,13 +155,15 @@ void loop()
             }
             else if (Time1 >= 3)
             {
-                //1번 바리게이트 내려옴
+                //3번 바리게이트 내려옴
                 for (pos_3 = 0; pos_3 <= degree; pos_3 -= 1)
                 {
                     myservo_3.write(pos_3);
                 }
+                l1 = 0;
+                Time1 = 0;
+                break;
             }
         }
-        break;
     }
 }
