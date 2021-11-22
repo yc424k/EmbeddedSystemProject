f88814c9a0a66b8cd01e7fc6c07aadebea01284c
sg1128 <onb01579@naver.com>
2021-11-22 15:44:06 +0900

    슬레이브 신호등

--- src/trafficlignt2.cpp
+++ src/trafficlignt2.cpp
@@ -1,30 +1,37 @@
-#include <Adafruit_NeoPixel.h>
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(12, 13);
-#define CAR_PIN1 2
+#include <Adafruit_NeoPixel.h> //네오픽셀
+#include <Wire.h> //I2C 통신
+#define sv 8
+#define CAR_PIN1 2 //차량
 #define CAR_PIN2 5
-#define PEO_PIN1 3
+#define PEO_PIN1 3 //횡단보도
 #define PEO_PIN2 4
 #define PEO_PIN3 6
 #define PEO_PIN4 7
+#define DLE_PIN1 8 //딜레마
+#define DLE_PIN2 9
 unsigned long l1,l2;
-unsigned long interval= 1024;
-int Time2,Time4,Time6;
+unsigned long interval= 1024; //1초
+int Time1,Time3,Time5;
 byte c;
-Adafruit_NeoPixel pixels1(4, CAR_PIN1, NEO_GRB + NEO_KHZ800);
+byte data; //마스터 한테 보낼 데이터
+Adafruit_NeoPixel pixels1(4, CAR_PIN1, NEO_GRB + NEO_KHZ800);  //네오픽셀 초기화
 Adafruit_NeoPixel pixels2(4, CAR_PIN2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels3(2, PEO_PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels4(2, PEO_PIN2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels5(2, PEO_PIN3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels6(2, PEO_PIN4, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels7(4, DLE_PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels8(4, DLE_PIN2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Time2=0;
-  Time4=0;
-  Time6=0;
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  Wire.begin(sv); //I2C 시작
+  Time1=0;
+  Time3=0;
+  Time5=0;
   l1=0;
-  mySerial.begin(9600);
-  pixels1.begin();
+  pixels1.begin();  //네오픽셀 시작
   pixels2.begin();
   pixels3.begin();
   pixels4.begin();
@@ -36,53 +43,59 @@ void setup() {
   pixels4.setPixelColor(1,255,0,0);
   pixels5.setPixelColor(1,255,0,0);
   pixels6.setPixelColor(1,255,0,0);
-  pixels1.show();
+  pixels1.show();  //네오픽셀 LED ON
   pixels2.show();
   pixels3.show();
   pixels4.show();
   pixels5.show();
   pixels6.show();
-  mySerial.write('1');
+  data = '1';
 }
 void loop() {
   l2=millis();
-  if (mySerial.available()) {
-  c = mySerial.read();
-  }
+  if(Wire.available()){
+      c = Wire.read();
+    }
   switch(c){
-  case '2':
-if(l2-l1 >= interval){
+  case '2': //차량
+    if(l2-l1 >= interval){
     l1=l2;
-    Time2++;
-    pixels1.clear();
-    if(Time2<= 3){
+    Time1++;
+    pixels1.clear();  //네오픽셀 LED OFF
+    if(Time1<=10){  //10초동안 초록색 파란색 LED ON
       pixels1.setPixelColor(0,0,255,0);
       pixels1.setPixelColor(1,0,0,255);
       pixels1.show();
-      }else if(Time2>3 && Time2 <= 5){
+      if(Time1>5){  //5초에서 10초사이에 딜레마 LED ON
+        theaterChase(512,2); //깜박이는 함수
+      }
+      }else if(Time1>10 && Time1 <= 15){ //10초부터 5초동안 주황색 LED ON
         pixels1.clear();
         pixels1.setPixelColor(2,255,215,0);
         pixels1.show();
-      }else{
+      }else{ //되돌아 오기 전까지 빨간색 LED ON
         pixels1.clear();
         pixels1.setPixelColor(3,255,0,0);
         pixels1.show();
-        mySerial.write('3');
+        data = '3'; //송신
         l1=0;
-        Time4=0;
+        Time3=0;
         break;
       }
     }
-    case '4':
+    case '4': //차량
     if(l2-l1 >= interval){
     l1=l2;
-    Time4++;
+    Time3++;
     pixels2.clear();
-    if(Time4<= 3){
+    if(Time3<= 3){
       pixels2.setPixelColor(0,0,255,0);
       pixels2.setPixelColor(1,0,0,255);
       pixels2.show();
-      }else if(Time4>3 && Time4 <= 5){
+      if(Time1>5){  //5초에서 10초사이에 딜레마 LED ON
+        theaterChase(512,3); //깜박이는 함수
+        }
+      }else if(Time3>3 && Time3 <= 5){
         pixels2.clear();
         pixels2.setPixelColor(2,255,215,0);
         pixels2.show();
@@ -90,32 +103,32 @@ if(l2-l1 >= interval){
         pixels2.clear();
         pixels2.setPixelColor(3,255,0,0);
         pixels2.show();
-        mySerial.write('5');
-        c='6';
+        data='5'; //송신
+        c = '6';
         l1=0;
-        Time6=0;
+        Time5=0;
         break;
       }
     }
-    case '6':
+    case '6': //횡단보도
     if(l2-l1 >= interval){
     l1=l2;
-    Time6++;
+    Time5++;
     pixels3.clear();
     pixels4.clear();
     pixels5.clear();
     pixels6.clear();
-    if(Time6<= 3){
+    if(Time5<= 3){
       pixels3.setPixelColor(0,0,255,0);
       pixels4.setPixelColor(0,0,255,0);
       pixels5.setPixelColor(0,0,255,0);
       pixels6.setPixelColor(0,0,255,0);
-    pixels3.show();
-    pixels4.show();
-    pixels5.show();
-    pixels6.show();
-    }else if(Time6>3 && Time6 <=10){
-      theaterChase(400);
+      pixels3.show();
+      pixels4.show();
+      pixels5.show();
+      pixels6.show();
+    }else if(Time5>3 && Time5 <=10){
+      theaterChase(512,1);
     }else{
       pixels3.clear();
       pixels4.clear();
@@ -129,31 +142,69 @@ if(l2-l1 >= interval){
       pixels4.show();
       pixels5.show();
       pixels6.show();
-      c='1';
+      c='1'; //다시 처음으로 
+      Time1=0;
       l1=0;
-      Time2=0;
       break;
       }
     }
   }
 }
 
-void theaterChase(int wait) {
-      for (int b=0; b < 3; b++) {
-        pixels3.clear();
+void theaterChase(int wait,int i) { //깜박이는 함수
+  switch(i){
+    case 1:
+      for (int b=0; b < 2; b++) { //b가 증가하면 깜박이는 시간이 길어진다
+      	pixels3.clear();
         pixels4.clear();
         pixels5.clear();
         pixels6.clear();
-        for (int c=b; c < 1; c=c+3) {
-          pixels3.setPixelColor(c, 0,255,0);
-          pixels4.setPixelColor(c, 0,255,0); 
-          pixels5.setPixelColor(c, 0,255,0); 
-          pixels6.setPixelColor(c, 0,255,0); 
+      	for (int c=b; c < 1; c=c+3) {
+      	  pixels3.setPixelColor(0, 0,255,0);
+          pixels4.setPixelColor(0, 0,255,0); 
+          pixels5.setPixelColor(0, 0,255,0); 
+          pixels6.setPixelColor(0, 0,255,0); 
       }
         pixels3.show();
         pixels4.show();
         pixels5.show();
         pixels6.show();
         delay(wait);
-  } 
+  	}
+    break;
+    case 2:
+    for (int b=0; b < 2; b++) {
+      	pixels7.clear();
+      	for (int c=b; c < 1; c=c+3) {
+      	  pixels7.setPixelColor(0, 0,0,255);
+          pixels7.setPixelColor(1, 0,0,255);
+          pixels7.setPixelColor(2, 0,0,255);
+          pixels7.setPixelColor(3, 0,0,255);
+        }
+        pixels7.show();
+        delay(wait);
+    }
+    break;
+    case 3:
+    for (int b=0; b < 2; b++) {
+        pixels8.clear();
+      	for (int c=b; c < 1; c=c+3) {
+          pixels8.setPixelColor(0, 0,0,255); 
+          pixels8.setPixelColor(1, 0,0,255); 
+          pixels8.setPixelColor(2, 0,0,255); 
+          pixels8.setPixelColor(3, 0,0,255); 
+      }
+        pixels8.show();
+        delay(wait);
+    }
+    break;
+  }
+}
+void requestEvent() {
+  Wire.write(data);
+}
+void receiveEvent(int howMany) {
+  if(Wire.available()) {
+    c = Wire.read();
+  }
 }
